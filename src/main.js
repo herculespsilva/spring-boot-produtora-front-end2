f71ff0ee16c9aa1c04280ef427540e6484f0b7fc
herculespsilva <herculespsilva@hotmail.com>
2020-11-27 01:59:42 +0000

    Mapeando classes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://fd68f96b-c488-4367-8220-34962728eaa9.ws-us02.gitpod.io' // url base do  serviço
+
+axios.interceptors.request.use(config => {              // incluindo um headers em toda requisição, token
+ config.headers.Authorization = 'um token'              // Basic base64(usuario:senha)
+ return config
+})
 
 Vue.config.productionTip = false
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,18 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state: {              //quando logar, coloca o usuario e senha para montar a requisição
+    usuario: '',        //modelo basic
+    senha: '',
+    permiteNavegacao: true
   },
   mutations: {
+      setUsuario (state, valor) {
+          state.usuario = valor;
+      },
+      setSenha(state, valor) {
+          state.senha = valor;
+      }
   },
   actions: {
   },
